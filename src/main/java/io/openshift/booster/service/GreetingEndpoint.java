1bfb6ca82ce795a3760141f5f06038ea457d6595
shotii <s.hoti@hotmail.de>
2018-08-31 09:07:39 +0000

    First commit.

--- src/main/java/io/openshift/booster/service/GreetingEndpoint.java
+++ src/main/java/io/openshift/booster/service/GreetingEndpoint.java
@@ -29,7 +29,7 @@ public class GreetingEndpoint {
     @GET
     @Produces("application/json")
     public Greeting greeting(@QueryParam("name") @DefaultValue("World") String name) {
-        final String message = String.format(Greeting.FORMAT, name);
+        final String message = String.format(Greeting.FORMAT, name + " from Sazan Hoti!");
         return new Greeting(message);
     }
 }
