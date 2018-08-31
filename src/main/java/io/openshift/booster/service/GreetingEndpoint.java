3d23034e6a4515c58b9bbe0dd3813b543d9220f5
shotii <s.hoti@hotmail.de>
2018-08-31 10:35:22 +0000

    iuz

--- src/main/java/io/openshift/booster/service/GreetingEndpoint.java
+++ src/main/java/io/openshift/booster/service/GreetingEndpoint.java
@@ -15,7 +15,7 @@
  */
 package io.openshift.booster.service;
 
-import javax.ws.rs.DefaultValue;
+import javax.ws.rs.DefaultValue; // HI
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
